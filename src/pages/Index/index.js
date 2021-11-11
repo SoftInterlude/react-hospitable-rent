cc2a5b7797bb3e3060be179eae6604a1a6611dd5
gejuncheng <juncheng.ge@outlook.com>
2021-11-11 21:42:08 +0800

    add carousel for home index page

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Carousel } from 'antd-mobile';
+import Axios from 'axios';
+
+const baseUrl = 'http://localhost:8080'
 
 export default class Index extends React.Component {
+  state = {
+    data: [],
+    imgHeight: 212,
+  }
+
+  getImgList = async () => {
+    const { data: res } = await Axios.get(`${baseUrl}/home/swiper`)
+    this.setState({
+      data: res.body
+    })
+  }
+
+  componentDidMount() {
+    this.getImgList()
+  }
+
+
   render() {
-    return <h2>这是首页</h2>
+    return (
+      <Carousel
+        autoplay
+        infinite
+      >
+        {this.state.data.map(item => (
+          <a
+            key={item.id}
+            href="#"
+            style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+          >
+            <img
+              src={`${baseUrl}${item.imgSrc}`}
+              alt=""
+              style={{ width: '100%', verticalAlign: 'top' }}
+              onLoad={() => {
+                // fire window resize event to change height
+                window.dispatchEvent(new Event('resize'));
+                this.setState({ imgHeight: 'auto' });
+              }}
+            />
+          </a>
+        ))}
+      </Carousel>
+    );
   }
 }
